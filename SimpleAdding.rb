6dcb51907ac2b26311c01e1f224d8411410dcf6d
Cho Jehum <jehum1@naver.com>
2015-07-31 15:40:59 +0900

    Create SimpleAdding.rb

---
+++ SimpleAdding.rb
@@ -0,0 +1,17 @@
+def SimpleAdding(num)
+
+  a = 0
+  1.upto(num) do |x|
+    a = a + x
+  end
+  # code goes here
+  return a
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+SimpleAdding(STDIN.gets)  
+
+
+
